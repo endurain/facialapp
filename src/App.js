b0315bd8e64ce975beb231a6d7eb2c86b78bcd7a
endurain <zacharyjsanders@gmail.com>
2018-06-27 11:44:20 -0600

    info

--- src/App.js
+++ src/App.js
@@ -31,7 +31,15 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    console.log('click')
+    console.log('click');
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+      function(response) {
+        // do something with response
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   }
 
   render() {
