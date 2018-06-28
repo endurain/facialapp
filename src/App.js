a59cd45078c34b392da6f54b14119762be18de29
endurain <zacharyjsanders@gmail.com>
2018-06-28 14:28:25 -0600

    adding bounding box css files

--- src/App.js
+++ src/App.js
@@ -35,7 +35,20 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
 
+  displayFace = (box) => {
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
@@ -48,13 +61,8 @@ class App extends Component {
     .predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(
-      function(response) {
-        this.calculateFaceLocation(response);
-      },
-      function(err) {
-      }
-    );
+    .then(response => this.displayFace(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err)); 
   }
 
   render() {
@@ -70,7 +78,7 @@ class App extends Component {
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}
          />
-         <FaceRecognition imageUrl={this.state.imageUrl}/>
+         <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
