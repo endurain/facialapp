055dccc592d451d2b5921b63a69031d0c045e136
zac <zacharyjsanders@gmail.com>
2018-07-01 21:57:19 -0600

    formatting

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,8 @@ import './Logo.css';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 65 }} style={{ height: 160, width: 160 }} >
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 65 }}
+				 style={{ height: 160, width: 160 }} >
  				<div className="Tilt-inner pa2">
  					<img style={{paddingTop: '12px'}}alt='' src={ai}/>
  				</div>
