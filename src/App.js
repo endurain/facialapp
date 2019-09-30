2d007795c9410f2cc65378fb609775f4b2f6d2b0
Zac Sanders <37851511+endurain@users.noreply.github.com>
2019-09-29 21:13:20 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+
 import './App.css';
 
 const app = new Clarifai.App({
